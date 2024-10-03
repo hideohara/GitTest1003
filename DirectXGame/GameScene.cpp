d0b48ff64934cac9403bcdf490e084247bfbd27f
ohara <ohara@g.neec.ac.jp>
2024-10-03 15:35:32 +0900

    Start

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,79 @@
+#include "GameScene.h"
+//#include "TextureManager.h"
+#include <cassert>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	delete model_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+
+	// ビュープロジェクションの初期化
+	//viewProjection_.Initialize();
+	camera_.Initialize();
+
+	// 3Dモデルの生成
+	model_ = Model::CreateFromOBJ("cube");
+
+	// ワールド変換の初期化
+	worldTransform_.Initialize();
+}
+
+void GameScene::Update() {
+	// 行列を更新
+	worldTransform_.TransferMatrix();
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	
+	// 3Dモデルを描画
+	model_->Draw(worldTransform_, camera_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
